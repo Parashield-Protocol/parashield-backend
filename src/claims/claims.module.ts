78af7428ad8121c6b0c18a43880e06a8f40a5762
Nonso Bethel <nonsobethel0@gmail.com>
2026-06-01 15:30:00 +0100

    feat(claims): update ClaimsModule imports to include PrismaModule

--- src/claims/claims.module.ts
+++ src/claims/claims.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ClaimsService }    from './claims.service';
 import { ClaimsController } from './claims.controller';
 import { ClaimsWorker }     from './claims.worker';
+import { PrismaModule }     from '../prisma/prisma.module';
 
 @Module({
+  imports:     [PrismaModule],
   controllers: [ClaimsController],
   providers:   [ClaimsService, ClaimsWorker],
   exports:     [ClaimsService],
