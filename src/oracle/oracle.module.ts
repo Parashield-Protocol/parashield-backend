58230831d544e33e06358837941a26da85730f38
Nonso Bethel <nonsobethel0@gmail.com>
2026-06-04 15:00:00 +0100

    feat(oracle): update OracleModule to import PrismaModule and StellarModule

--- src/oracle/oracle.module.ts
+++ src/oracle/oracle.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { OracleService }    from './oracle.service';
 import { OracleController } from './oracle.controller';
 import { OracleWorker }     from './oracle.worker';
+import { PrismaModule }     from '../prisma/prisma.module';
+import { StellarModule }    from '../stellar/stellar.module';
 
 @Module({
+  imports:     [PrismaModule, StellarModule],
   controllers: [OracleController],
   providers:   [OracleService, OracleWorker],
   exports:     [OracleService],
