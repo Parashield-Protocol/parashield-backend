0e5c33a3dd5808cf378dbc06b6d1bd8926d6cc47
Jaydbrown <jaiyeolawety705@gmail.com>
2026-06-26 10:42:44 +0100

    import OracleModule and PolicyModule into ClaimsModule

--- src/claims/claims.module.ts
+++ src/claims/claims.module.ts
@@ -3,9 +3,11 @@ import { ClaimsService }    from './claims.service';
 import { ClaimsController } from './claims.controller';
 import { ClaimsWorker }     from './claims.worker';
 import { PrismaModule }     from '../prisma/prisma.module';
+import { OracleModule }     from '../oracle/oracle.module';
+import { PolicyModule }     from '../policy/policy.module';
 
 @Module({
-  imports:     [PrismaModule],
+  imports:     [PrismaModule, OracleModule, PolicyModule],
   controllers: [ClaimsController],
   providers:   [ClaimsService, ClaimsWorker],
   exports:     [ClaimsService],
